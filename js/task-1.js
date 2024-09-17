ceb39739f55c4ee882e3ebea8339015970a12cc9
Suntrotter <129418123+Suntrotter@users.noreply.github.com>
2024-09-17 19:23:41 +0300

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const categories = document.querySelectorAll('li.item');
+
+
+console.log(`Number of categories: ${categories.length}`);
+
+
+categories.forEach(category => {
+  
+  const title = category.querySelector('h2').textContent;
+  const items = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${items}`);
+});
